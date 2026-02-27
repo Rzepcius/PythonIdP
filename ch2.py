d6cb217fa0ac8f14db967a097211bd0a4c707ad1
andrzej.rzepecki <rzepcius@gmail.com>
2026-02-27 10:05:18 +0100

    strings

---
+++ ch2.py
@@ -0,0 +1,20 @@
+firstName = "John"
+lastName = "Doe"
+fullName = f"{firstName} {lastName}"
+print(fullName)
+message=(f"Witaj {firstName} {lastName}!")
+print(message)
+print("Pytong")
+print("\tPytong")
+print("\t\nPytong")
+msg2 = " Pytong    "
+print(msg2)
+print(msg2.rstrip())
+print(msg2)
+msg2=msg2.rstrip()
+print(msg2)
+msg2=msg2.strip()
+print(msg2)
+url="https://www.onet.pl"
+url=url.removeprefix("https://")
+print(url)
