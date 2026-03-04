72996cde4d08c2e7383ec6dd7abb42a74447f255
andrzej.rzepecki <rzepcius@gmail.com>
2026-03-04 12:09:51 +0100

    list work

---
+++ ch4/listWork.py
@@ -0,0 +1,20 @@
+magicians = ['alicja', 'dawid', 'karolina']
+for magician in magicians:
+    print (magician)
+for magician in magicians:
+    print (f"{magician.title()}, to była doskonała sztuczka!")
+for magician in magicians:
+    print(f"{magician.title()}, to była doskonała sztuczka!")
+    print(f"Nie mogę się doczekać Twojej kolejnej sztuczki,{magician.title()}.\n")
+print("Dziękuję wszystkim. To był naprawdę wspaniały występ!")
+for value in range(1, 5):
+    print(value)
+numbers = list(range(1, 6))
+print(numbers)
+even_numbers = list(range(2, 11,2))
+print(even_numbers)
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append (square)
+print (squares)
