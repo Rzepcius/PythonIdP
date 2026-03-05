ae6fd5f5269deaae4fc909016e0ceefef89e5736
Rzepciu <rzepcius@gmail.com>
2026-03-05 09:01:55 +0100

    ch4

--- ch4/listWork.py
+++ ch4/listWork.py
@@ -18,3 +18,41 @@ for value in range(1, 11):
     square = value ** 2
     squares.append (square)
 print (squares)
+min(numbers)
+max(numbers)
+sum(numbers)
+squares2=[value**2 for value in range(1, 30)]
+print(squares2)
+print(squares2[0:3])
+print(squares2[11:22])
+print(squares2[:8])
+print(squares2[25:])
+print(squares2[-3:])
+players = ['karol', 'martyna', 'michał', 'florian', 'ela']
+
+print("Oto trzech pierwszych graczy naszej drużyny:")
+for player in players [:3]:
+    print(player.title())
+
+my_foods = ['pizza', 'falafel', 'ciasto z marchwi']
+friend_foods = my_foods [:]
+my_foods.append("pieróg")
+friend_foods.append("zapiekanka")
+
+print ("Moje ulubione potrawy to:")
+print (my_foods)
+
+print("\nUlubione potrawy mojego przyjaciela to:")
+print(friend_foods)
+
+dimensions = (200, 50)
+print (dimensions[0])
+print(dimensions[1])
+# dimensions[0] = 250
+for dimension in dimensions:
+    print (dimension)
+
+dimensions = (400, 100)
+print("\nWymiary po modyfikacji:")
+for dimension in dimensions:
+    print(dimension)
